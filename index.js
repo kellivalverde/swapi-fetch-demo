40825651073a44b67f49d53c69bf76e00ffaf9fe
kellivalverde <paughka@gmail.comls>
2020-03-08 15:30:04 -0400

    work in progress

--- index.js
+++ index.js
@@ -26,4 +26,55 @@ fetch('https://swapi.co/api/people')
     });
    })
    .catch(err => console.log(err));
-  
+
+
+
+
+
+
+   //from a different site
+   /**
+ * Load Json File example
+ */
+const loadJson = () => {
+    return fetch('movies.json')
+      .then((response) => {
+        loadMenu();
+        return response.json()
+      })
+      .then((res) => {
+        const moviesList = document.querySelector('ul');
+        for (let i = 0; i < res.movies.length; i++) {
+          const movie = document.createElement('li');
+          movie.innerHTML = `<h3>${res.movies[ i ].title}</h3>`;
+          movie.innerHTML += `<p> Episode ${res.movies[ i ].episode_number}</p>`;
+          movie.innerHTML += `<p> ${res.movies[ i ].description}</p>`;
+          let castList = '<ul>';
+          res.movies[ i ].main_characters.forEach((character) => {
+            castList += `<li><img src="/images/falcon.png" class="falcon">${character}</li>`;
+          })
+          castList += '</ul>'
+          movie.innerHTML += ` Cast: ${castList}`;
+          moviesList.appendChild(movie);
+        }
+      })
+  }
+  /**
+ * HTML menu response example (So nice web sample)
+ */
+const loadMenu = () => {
+    const menu = document.getElementById('menu');
+    fetch('.menu.html')
+      .then(function (response) { return response.text() })
+      .then(function (text) {
+        menu.innerHTML = text;
+        const burger = document.getElementById('burger');
+        const nav = document.getElementById('nav');
+        if (burger) {
+          burger.onclick = () => {
+            burger.classList.toggle('active');
+            nav.classList.toggle('show');
+          }
+        }
+      });
+  }
